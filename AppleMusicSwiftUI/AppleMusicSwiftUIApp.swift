fb9013d0e0a8a9d1c09b3d417825dfb74138d86a
elenoack <elenanoack@mail.ru>
2022-08-27 19:30:46 +0300

    Initial Commit

---
+++ AppleMusicSwiftUI/AppleMusicSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusicSwiftUIApp.swift
+//  AppleMusicSwiftUI
+//
+//  Created by Elena Noack on 27.08.22.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusicSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
